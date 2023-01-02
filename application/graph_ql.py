f024d40cb18125765000908ff5803b6b4c6ff138
PARADIGMA\QiDimMak <jorgecardona@utp.edu.co>
2023-01-01 23:26:04 -0500

    adicionado listar y adicionar con graphql

--- application/graph_ql.py
+++ application/graph_ql.py
@@ -1,8 +1,18 @@
 # uvicorn graph_ql:app --host localhost --reload --port 5555
 # http://localhost:5555/graphql, en postman siempre metodo post
 """ 
-query user_info{
-  user{
+query listUserByName{
+  userList{
+    name
+    }
+},
+query listUserByAge{
+  userList{
+    age
+    }
+},
+mutation addUser{
+  addUserFunction(name:"uno",age:25){
     name
   }
 }
@@ -11,7 +21,7 @@ query user_info{
 import strawberry
 from fastapi import FastAPI
 from strawberry.asgi import GraphQL
-
+from typing import List
 
 
 @strawberry.type
@@ -20,15 +30,25 @@ class User:
     age: int
 
 
+user = [User(name="Patrick", age=100), User(name="July", age=22)]
+
 @strawberry.type
 class Query:
     @strawberry.field
-    def user(self) -> User:
-        return User(name="Patrick", age=100)
+    def user_list(self) -> List[User]: # retorna la lista de usuarios creados
+        return user
 
 
-schema = strawberry.Schema(query=Query)
 
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    def add_user_function(self, name: str, age: int) -> User: # retorna solo el usuario creado
+        user_added = User(name=name, age=age)
+        user.append(user_added)
+        return user_added
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
 
 graphql_app = GraphQL(schema)
 
