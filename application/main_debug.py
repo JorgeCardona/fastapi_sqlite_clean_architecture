583853951ccec96d1da89adcc9011c4631dcf3fc
PARADIGMA\QiDimMak <jorgecardona@utp.edu.co>
2023-01-09 21:19:31 -0500

    SCRIPT PARA MODO DEBUG

--- application/main_debug.py
+++ application/main_debug.py
@@ -1,5 +1,6 @@
 from configuration.fastapi.fast_api_configuration import API_APP, API_HOST, API_PORT
 
+# INICIAR ESTE SCRIPT EN MODO DEBUG PARA DEBUGGEAR LA APLICACION
 import uvicorn
 if __name__ == "__main__":
     uvicorn.run(app=API_APP, 
