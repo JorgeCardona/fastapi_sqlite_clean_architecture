ecb654ba49b19b4a51b1b183ad3a4745e9809148
PARADIGMA\QiDimMak <jorgecardona@utp.edu.co>
2023-01-14 01:00:03 -0500

    adicionado carpeta personalizada para cada recurso creado

---
+++ resources/resources_generator copy.py
@@ -0,0 +1,240 @@
+class CreateResources:
+    
+    def __init__(self, name='application', application_type='REST'):
+        
+        self.application_directory_name = name
+        self.application_application_type = str(application_type).upper()
+        
+        self.configurations_directory = f'{self.application_directory_name}/configuration'
+        self.domain_directory         = f'{self.application_directory_name}/domain'
+        self.entities_directory       = f'{self.domain_directory}/entities'
+        self.models_directory         = f'{self.entities_directory}/models'
+        self.schemas_directory        = f'{self.entities_directory}/schemas'
+        self.interfaces_directory     = f'{self.domain_directory}/interfaces'
+        self.repositories_directory   = f'{self.interfaces_directory}/repositories'
+        self.business_directory       = f'{self.interfaces_directory}/business'
+        self.usecases_directory       = f'{self.application_directory_name}/usecases'
+        self.services_directory       = f'{self.application_directory_name}/services'
+        self.basic_directory          = ['test',
+                                        'deployment',
+                                        'requirements']
+        
+        self.scaffolding_directories_list = [self.usecases_directory, self.services_directory]
+        
+        # dictionary for creates resource
+        self.resources ={
+            'entity':{
+                'prefix_directory': ['entity_', 'entity_'],
+                'directory':[self.entities_directory, self.entities_directory],
+                'suffix': ['_model.py','_schema.py'],
+                'template':[]
+            },
+            'model':{
+                'prefix_directory': ['entity_'],
+                'directory':[self.entities_directory],
+                'suffix': ['_model.py'],
+                'template':['code_templates/09_model_template.py']
+            },            
+            'schema':{
+                'prefix_directory': ['entity_'],
+                'directory':[self.entities_directory],
+                'suffix': ['_schema.py'],
+                'template':[]
+            },
+            'interface':{
+                'prefix_directory': ['repository_', 'business_'], 
+                'directory':[self.repositories_directory, self.business_directory],
+                'suffix': ['_repository.py', '_business.py'],
+                'template':[]
+            },            
+            'repository':{
+                'prefix_directory': ['repository_'],                
+                'directory':[self.repositories_directory],
+                'suffix': ['_repository.py'],
+                'template':[]
+            },    
+            'business':{
+                'prefix_directory': ['business_'],                
+                'directory':[self.business_directory],
+                'suffix': ['_business.py'],
+                'template':[]
+            },   
+            'usecase':{
+                'prefix_directory': ['usecase_'],
+                'directory':[self.usecases_directory],
+                'suffix': ['_usecase.py'],
+                'template':[]
+            },    
+            'service':{
+                'prefix_directory': ['service_'],
+                'directory':[self.services_directory],
+                'suffix': ['_service.py'],
+                'template':[]
+            },  
+            'all':{
+                'directory':[self.entities_directory, 
+                             self.entities_directory,
+                             self.repositories_directory, 
+                             self.business_directory, 
+                             self.usecases_directory, 
+                             self.services_directory
+                            ],
+                'prefix_directory': ['entity_', 
+                                     'entity_',
+                                     'repository_', 
+                                     'business_',
+                                     'usecase_',
+                                     'service_'
+                                     ],
+                'suffix': [
+                           '_model.py',
+                           '_schema.py',
+                           '_repository.py',
+                           '_business.py',
+                           '_usecase.py',
+                           '_service.py'                           
+                           ],
+                'template':[]
+            },  
+        }
+                 
+    def __basics_directories_list__(self):
+        
+        self.scaffolding_directories_list.extend(self.basic_directory)
+        return [self.basic_directory]
+
+    def __configurations_directories_list__(self):
+        
+        api_type_directory = '/graphql' if self.application_application_type == 'GRAPHQL' else '/rest'
+        
+        configurations_directories = [
+                       api_type_directory,
+                       '/environment',
+                       '/database',
+                       '/utils',
+                       '/cors',
+                       '/log'                       
+                       ]
+        
+        directories = [self.configurations_directory + directory for directory in  configurations_directories]
+        # add configurations_directories_list
+        self.scaffolding_directories_list.extend(directories)
+        
+        return [self.configurations_directory + directory for directory in directories]
+        
+    def __domain_directories_list__(self):
+        
+        directories = [
+                       self.entities_directory,
+                       self.repositories_directory,
+                       self.business_directory,
+                      ]
+
+        # add configurations_directories_list
+        self.scaffolding_directories_list.extend(directories)
+        
+        return directories
+
+    def __scaffolding_directories_list__(self):
+
+        # load every list from every function and add to scaffolding list
+        self.__basics_directories_list__()
+        self.__configurations_directories_list__()
+        self.__domain_directories_list__  ()     
+        
+        # order list by name
+        self.scaffolding_directories_list.sort()
+        
+        return self.scaffolding_directories_list
+
+    def create_file(self, file, template=None):
+        
+        """_summary_
+        create every python file required
+
+        Args:
+            file (_type_): name of file to create
+            template (_type_): create content into the file using template for it
+        """
+        
+        from os import path
+
+        # if exists file does not replace it
+        if not path.exists(file):            
+            with open(f'{file}','w') as file_create:
+                pass
+        
+    def create_scaffolding(self):
+        """_summary_
+
+        creates every directory defined for clean architecture
+        Returns:
+            _type_: successfull mesage
+        """
+        from os import makedirs
+        
+        directories_list = self.__scaffolding_directories_list__()
+
+        for directory in directories_list:
+            
+            # creates every directory on the list, if a directory exists ignore creating it
+            makedirs(directory, exist_ok=True)
+            
+            #  if the directory is inside the source code folder, creates __init__.py
+            if directory.__contains__('/'):
+                self.create_file(f'{directory}/__init__.py')    
+
+        return 'All Directories were created Successfully'
+        
+    def create_resource(self, resource_type, file_name):
+        """_summary_
+        Creates every resource python file into folder
+        it depends from resource_type
+
+        Args:
+            resource_type (_type_): every diferent resource type
+            file_name (_type_): name of file to create
+        """
+        
+        dictionary = self.resources.get(resource_type)
+        
+        directory = dictionary.get('directory')
+        resource_name =  dictionary.get('suffix')
+        file = str(file_name).lower()
+        prefix_directory = dictionary.get('prefix_directory')
+        
+        self.create_scaffolding()
+        
+        for index, directory_value in enumerate(directory):
+            
+            # crea el nombre del directorio basado en el nombre del archivo
+            final_directory = f'{directory_value}/{prefix_directory[index]}{file}' 
+            # si los archivos estan dentro de un mismo directorio con nombre unificado
+
+            from os import makedirs              
+            makedirs(final_directory, exist_ok=True)
+
+            self.create_file(f'{final_directory}/{file}{resource_name[index]}')
+     
+    def create_resource_from_template(self, resource_type, file_name):
+        """_summary_
+        Creates every resource python file into folder
+        it depends from resource_type
+
+        Args:
+            resource_type (_type_): every diferent resource type
+            file_name (_type_): name of file to create
+        """
+        
+        dictionary = self.resources.get(resource_type)
+        
+        directory = dictionary.get('directory')
+        resource_name =  dictionary.get('suffix')
+        file = str(file_name).lower()
+        template_base = dictionary.get('template')
+        
+        self.create_scaffolding()
+        
+        for index, directory_value in enumerate(directory):
+
+            self.create_file(file=f'{directory_value}/{file}{resource_name[index]}', template=template_base)
